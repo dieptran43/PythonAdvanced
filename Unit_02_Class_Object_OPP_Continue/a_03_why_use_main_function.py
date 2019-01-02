f447dff7bc279365172022a559d8214b67c56ad1
dieple <dieple@kootoro.com>
2019-01-02 14:31:46 +0700

    example about when use __main__  in python

---
+++ Unit_02_Class_Object_OPP_Continue/a_03_why_use_main_function.py
@@ -0,0 +1,8 @@
+def sumInfo(a, b):
+    print("Sum: ", a +b)
+
+#use __main__
+if __name__ == "__main__":
+    sumInfo(4,5)
+
+sumInfo(4,6)
---
+++ Unit_02_Class_Object_OPP_Continue/a_04_why_use_main_function_test.py
@@ -0,0 +1,4 @@
+import a_03_why_use_main_function
+
+a_03_why_use_main_function.sumInfo(4,7)
+print(__name__)
